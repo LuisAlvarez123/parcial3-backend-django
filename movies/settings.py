abcb15febeb9d4330d926928122f5b538b6af560
LuisAlvarez123 <luisjoa@mesoamericana.edu.gt>
2021-11-28 12:04:53 -0800

    config static

--- movies/settings.py
+++ movies/settings.py
@@ -129,8 +129,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 GRAPHENE = {
     'SCHEMA': 'movies.schema.schema',    
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
