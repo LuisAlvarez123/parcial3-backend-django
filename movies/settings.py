d34c593700ace5780509dd67574e5576d69fbf8b
LuisAlvarez123 <luisjoa@mesoamericana.edu.gt>
2021-11-28 12:07:40 -0800

    config os

--- movies/settings.py
+++ movies/settings.py
@@ -130,12 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 GRAPHENE = {
     'SCHEMA': 'movies.schema.schema',    
